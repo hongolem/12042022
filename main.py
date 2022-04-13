3bd72652a69bd5090685b639c65754eeb4e4b837
JanHoly <53258234+hongolem@users.noreply.github.com>
2022-04-13 14:21:44 +0200

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-stred_randomizer = [3, 2]
+stred_randomizer = [randint(1, 3), randint(1, 3)]
 A1 = [0, 0]
 B1 = [4, 0]
 C1 = [4, 3]
@@ -16,14 +16,14 @@ def Plotting():
     led.plot(B1[0], B1[1])
     led.plot(C1[0], C1[1])
     led.plot(D1[0], D1[1])
-    A2 = []
-    B2 = []
-    C2 = [C1[0] - stred_randomizer[0], C1[1] - stred_randomizer[1]] #4-3, 3-2
-    D2 = []
-    #led.plot(A2[0], A2[1])
-    #led.plot(B2[0], B2[1])
+    A2 = [0, 4]
+    B2 = [1, 4]
+    C2 = [C1[0] - stred_randomizer[0], C1[1] - stred_randomizer[1]]
+    D2 = [3, 4]
+    led.plot(A2[0], A2[1])
+    led.plot(B2[0], B2[1])
     led.plot(C2[0], C2[1])
-    #led.plot(D2[0], D2[1])
+    led.plot(D2[0], D2[1])
 
 def stred():
     led.plot_brightness(stred_randomizer[0], stred_randomizer[1], 127)
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-let stred_randomizer = [3, 2]
+let stred_randomizer = [randint(1, 3), randint(1, 3)]
 let A1 = [0, 0]
 let B1 = [4, 0]
 let C1 = [4, 3]
@@ -14,17 +14,16 @@ function Plotting() {
     led.plot(B1[0], B1[1])
     led.plot(C1[0], C1[1])
     led.plot(D1[0], D1[1])
-    A2 = []
-    B2 = []
+    A2 = [0, 4]
+    B2 = [1, 4]
     C2 = [C1[0] - stred_randomizer[0], C1[1] - stred_randomizer[1]]
-    // 4-3, 3-2
-    D2 = []
-    // led.plot(A2[0], A2[1])
-    // led.plot(B2[0], B2[1])
+    D2 = [3, 4]
+    led.plot(A2[0], A2[1])
+    led.plot(B2[0], B2[1])
     led.plot(C2[0], C2[1])
+    led.plot(D2[0], D2[1])
 }
 
-// led.plot(D2[0], D2[1])
 basic.forever(function stred() {
     led.plotBrightness(stred_randomizer[0], stred_randomizer[1], 127)
 })
